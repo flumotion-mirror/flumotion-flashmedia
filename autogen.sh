61c6b9ff4afc668c1d13579e67df409948119fec
Xavier Queralt <xqueralt@flumotion.com>
2011-09-26 12:53:14 +0000

    Releasing 0.10.0 "Gyoza"

--- autogen.sh
+++ autogen.sh
@@ -17,12 +17,16 @@ if test ! -f common/flumotion-trial;
   exit 1
 fi
 
-autopoint || exit 1
+rm -f .version
+libtoolize -c --force || exit 1
+autopoint --force || exit 1
+cp -f common/intltool-Makefile.in.in po/Makefile.in.in
 aclocal -I m4 -I common || exit 1
 # libtoolize --force || exit 1
 # autoheader || exit 1
 autoconf || exit 1
 automake -a || exit 1
+
 echo "./autogen.sh $@" > autoregen.sh
 chmod +x autoregen.sh
 ./configure $@
